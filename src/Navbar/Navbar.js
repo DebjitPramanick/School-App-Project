2a60e5fb78fb93de0c21c5562783c74f6890d3b8
DebjitPramanick <dpramanick9990@gmail.com>
2021-03-21 16:52:33 +0530

    Screen 1

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,6 +6,16 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link } from "react-router-dom";
 import home from "../img/home.png"
+import notice from "../img/notice.png"
+import attendance from "../img/attendance.png"
+import grid from "../img/grid.png"
+import schedule from "../img/schedule.png"
+import fees from "../img/fees.png"
+import support from "../img/support.png"
+import flag from "../img/flag.png"
+import account from "../img/account.png"
+import calendar from "../img/calendar.png"
+import multimedia from "../img/multimedia.png"
 
 const Navbar = () => {
     return (
@@ -17,43 +27,37 @@ const Navbar = () => {
 
             <ul className="routes">
                 <li>
-                    <Link className="cur-route" to="/">
-                        <img src={home} alt="" />
-                        Home
-                    </Link>
+                    <Link className="cur-route" to="/"><img src={home} alt="" />Home</Link>
                 </li>
                 <li>
-                    <Link to="/">
-                        <img src={home} alt="" />
-                        Notice Board
-                    </Link>
+                    <Link to="/"><img src={notice} alt="" />Notice Board</Link>
                 </li>
                 <li>
-                    <Link to="/attendance"><img src={home} alt="" />Attendance</Link>
+                    <Link to="/attendance"><img src={attendance} alt="" />Attendance</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Fees Details</Link>
+                    <Link to="/"><img src={fees} alt="" />Fees Details</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Calendar</Link>
+                    <Link to="/"><img src={calendar} alt="" />Calendar</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Multimedia</Link>
+                    <Link to="/"><img src={multimedia} alt="" />Multimedia</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Time Tables</Link>
+                    <Link to="/"><img src={grid} alt="" />Time Tables</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Schedules</Link>
+                    <Link to="/"><img src={schedule} alt="" />Schedules</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Support Requests</Link>
+                    <Link to="/"><img src={support} alt="" />Support Requests</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Support</Link>
+                    <Link to="/"><img src={flag} alt="" />Support</Link>
                 </li>
                 <li>
-                    <Link to="/"><img src={home} alt="" />Account</Link>
+                    <Link to="/"><img src={account} alt="" />Account</Link>
                 </li>
             </ul>
 
